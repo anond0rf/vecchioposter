dd49b09f007f551f9ac0d20a6add1a5e1b70a83a
anond0rf <anondorf@protonmail.com>
2024-10-13 02:59:13 +0200

    docs(licenses): update third-party licenses attribution

--- main.go
+++ main.go
@@ -1,3 +1,5 @@
+// This file uses code from github.com/spf13/cobra, licensed under the Apache License, Version 2.0
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 package main
 
 import (
